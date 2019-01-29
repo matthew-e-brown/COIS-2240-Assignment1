63c2a9576a27e13b83a543bcbc73deaca589c16a
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-01-29 02:42:40 -0500

    Setup commit
    - Created IDEA Project
    - Typed up README.md
    - Added Assignment's PDF

---
+++ A1-Java/src/Main.java
@@ -0,0 +1,20 @@
+/* # Matthew Brown, #0648289
+ * Trent University, COIS-2240H: Assignment #1
+ * Fibonacci Sequence through iteration and recursion
+ *
+ * ## Resources used:
+ * Not all of these were used in the final project; these are just the things I looked up during development.
+ * ---
+ * https://stackoverflow.com/questions/2506077/how-to-read-integer-value-from-the-standard-input-in-java
+ * https://stackoverflow.com/a/351571/10549827
+ * https://stackoverflow.com/questions/30073980/java-writing-strings-to-a-csv-file
+ * https://stackoverflow.com/questions/7586266/is-chain-of-stringbuilder-append-more-efficient-than-string-concatenation/7586780
+ * https://stackoverflow.com/questions/3715967/when-should-we-call-system-exit-in-java
+ * ---
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Matt was here");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# COIS-2240-Assignment1
+# COIS-2240: Assignment #1
+
+This assignment requires us to generate the Fibonacci sequence up to a given `n` value. We must do so both iteratively and recursively.
+
+For example, running the function with a parameter of `n = 10` will return
+```
+>>> 0 1 1 2 3 5 8 13 21 34
+```
+
+We must run the program with `n` values from `10` to `50`, in `n = 10` intervals. We must log how long each of these calls take and create a graph of them. The assignment also includes using Git + Github for version control during the process of development. 
+
+Finally (technically, this was supposed to come first, but oh well), there is a small word-to-UML-Diagram problem to do.
