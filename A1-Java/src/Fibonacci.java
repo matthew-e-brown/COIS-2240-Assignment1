167188ac6c486150b1697fcbcb01f61577b164e5
Matthew Brown <matthew.e.brown.17@gmail.com>
2019-02-01 00:13:27 -0500

    Main functionality complete
    - Added helper functions to calculate elapsed time for the main Fibonacci methods
    - Added a non-graphical UI, allowing for several options
    - Added the following features, accessible through the UI:
    	- Test:		To generate the entire Fibonacci Sequence, to verify it is correct
    	- Run:		To time the calculation of the n-th term of the Fibonacci Sequence
    	- Export:	Run commmand, except with the inclusion of a .CSV export

--- A1-Java/src/Fibonacci.java
+++ A1-Java/src/Fibonacci.java
@@ -1,6 +1,6 @@
 class Fibonacci {
-    static int iterative(int n) {
-        int a = 0, b = 1, c = 1;
+    static long iterative(int n) {
+        long a = 0, b = 1, c = 1;
         for (int i = 0; i < n; i++) {
             a = b; // n becomes what n + 1 was;
             b = c; // n + 1 becomes what n + 2 was;
@@ -9,8 +9,24 @@ class Fibonacci {
         return a;
     }
 
-    static int recursive (int n) {
+    static long recursive (int n) {
         if (n == 0 || n == 1) return n; // The 0th and 1st terms ARE 0 and 1
         else return recursive(n - 1) + recursive(n - 2);
+    }
+
+    static long iterativeTest(int n) {
+        long[] times = new long[2];
+        times[0] = System.nanoTime();
+        iterative(n);
+        times[1] = System.nanoTime();
+        return times[1] - times[0];
+    }
+
+    static long recursiveTest(int n) {
+        long[] times = new long[2];
+        times[0] = System.nanoTime();
+        recursive(n);
+        times[1] = System.nanoTime();
+        return times[1] - times[0];
     }
 }
--- A1-Java/src/Main.java
+++ A1-Java/src/Main.java
@@ -11,28 +11,136 @@
  * https://stackoverflow.com/questions/7586266/is-chain-of-stringbuilder-append-more-efficient-than-string-concatenation/7586780
  * https://stackoverflow.com/questions/3715967/when-should-we-call-system-exit-in-java
  * ---
+ *
+ * ## Disclaimer:
+ * This program runs extremely inefficiently. This is only because of how it was asked to be done.
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.io.PrintWriter;
 
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int terms = 0;
-        boolean validInput = false;
+        while (true) {
+            char input = getInput();
+            switch (input) {
+                case 't':
+                    Test();
+                    break;
+                case 'r':
+                    Run();
+                    break;
+                case 'e':
+                    try {
+                        Export();
+                    } catch (FileNotFoundException ex) {
+                        System.out.println(ex.toString());
+                        System.out.println("Please try again.");
+                    }
+                    break;
+                case 'q':
+                    System.exit(0);
+            }
+            System.out.print("Enter something to confirm and go again: ");
+            scan.nextLine();
+        }
+    }
+
+    private static char getInput() {
+        char input;
+        System.out.println("\nWhat would you like to do?");
+        do {
+            System.out.print("\nOptions are:\n" +
+                    "Test - Run the program in both modes, printing out all numbers up to a given n. To verify the output of the sequence.\n" +
+                    "Run - Run the program and time how long it takes to calculate a given nth term.\n" +
+                    "Export - Run the program, without printing, and export the time results to a CSV file.\n" +
+                    "Quit - Exit the program.\n\n" +
+                    "Enter your choice: ");
+            input = scan.nextLine().toLowerCase().charAt(0);
+        } while (input != 't' && input != 'r' && input != 'e' && input != 'q');
+        return input;
+    }
+
+    private static int getTerms() {
+        int terms = -1;
         do {
             try {
                 System.out.print("Enter the term number you'd like: ");
                 terms = scan.nextInt();
-                validInput = (terms >= 0);
             } catch (InputMismatchException ex) {
                 scan.nextLine();
                 System.out.println("Please enter a positive integer value.");
             }
-        } while(!validInput);
+        } while (terms < 0);
+        return terms;
+    }
 
-        System.out.println(Fibonacci.iterative(terms));
-        System.out.println(Fibonacci.recursive(terms));
+    private static void Test(){
+        int terms = getTerms();
+
+        System.out.println("\n\nIterative Test:");
+        for (int i = 0; i < terms; i++) {
+            System.out.print(Fibonacci.iterative(i) + " ");
+        }
+
+        System.out.println("\n\nRecursive Test:");
+        for (int i = 0; i < terms; i++) {
+            System.out.print(Fibonacci.recursive(i) + " ");
+        }
+    }
+
+    private static void Run() {
+        int terms = getTerms();
+        System.out.println("\nIterative Test:");
+        long elapsed = Fibonacci.iterativeTest(terms);
+        System.out.println("Elapsed time: " + elapsed + " nanoseconds");
+        System.out.println("\nRecursive Test:");
+        elapsed = Fibonacci.recursiveTest(terms);
+        System.out.println("Elapsed time: " + elapsed + " nanoseconds");
+    }
+
+    private static void Export() throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter(new File("data-export.csv"));
+        StringBuilder sb = new StringBuilder();
+        int t = 0;
+        do {
+            try {
+                System.out.print("Enter how many runs through would you like to do: ");
+                t = scan.nextInt();
+            } catch (InputMismatchException ex) {
+                scan.nextLine();
+                System.out.println("Please input an integer.");
+            }
+        } while (t <= 0);
+
+        sb.append("Call Type, Term Length, ");
+        for (int i = 1; i <= t; i++) {
+            sb.append(String.format("Run #%d, ", i));
+        }
+        sb.append("Average Time\n");
+
+        for (int ir = 0; ir <= 1; ir++) { //Do this twice; once for Iterative, once for Recursive
+            long[] el = new long[t];
+            for (int n = 10; n <= 50; n += 10) {
+                for (int i = 0; i < t; i++) {
+                    el[i] = (ir == 0) ? Fibonacci.iterativeTest(n) : Fibonacci.recursiveTest(n);
+                }
+                sb.append(String.format(((ir == 0) ? "Iterative" : "Recursive") + ", %d, ", n));
+                long sum = 0;
+                for (long l : el) {
+                    sb.append(String.format("%d, ", l));
+                    sum += l;
+                }
+                sum /= t; //sum is now avg.
+                sb.append(String.format("%d\n", sum));
+            }
+        }
+
+        pw.write(sb.toString());
+        pw.close();
     }
 }
